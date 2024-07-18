43cecbd98a40bd994ab781ee98c9e423c4956e4f
Denis Ruparel <deniskalpeshbhai436@gmail.com>
2024-07-18 22:07:56 +0530

    Patterns commit Day-4

--- Learn The Basics/Patterns/09_diamond.cpp
+++ Learn The Basics/Patterns/09_diamond.cpp
@@ -14,15 +14,18 @@ int main(){
         for ( j = 0; j < n-i-1; j++){
             cout << " ";
         }
-
-        // for ( j = 0; j < i; j++)
-        //     cout << " ";
+    cout << endl;
+    }
+    for ( i = 0; i < n; i++){
+        for ( j = 0; j < i; j++)
+            cout << " ";
         
         for ( j = 0; j < 2*n-(2*i+1); j++)
                 cout << "*";
 
         for ( j = 0; j < i; j++)
             cout << " ";
-    cout << endl;
+        
+        cout << endl;
     }
 }
