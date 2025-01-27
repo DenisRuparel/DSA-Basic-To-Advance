c888de5afbd6cc081b7224e7e4dad8cb69b11db1
Denis Ruparel <deniskalpeshbhai436@gmail.com>
2025-01-27 10:19:39 +0530

    LinkedList Day-31

---
+++ Linked List/Basics of 1D LL/02_Inserting_a_node.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+class Solution {
+  public:
+    Node *insertAtEnd(Node *head, int x) {
+        Node* newNode = new Node(x);
+        if(head == nullptr)
+            return newNode;
+        Node* mover = head;
+        while(mover->next != nullptr)
+            mover = mover->next;
+        mover->next = newNode;
+        return head;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        Node* head = nullptr;
+
+        if (!arr.empty()) {
+            head = new Node(arr[0]);
+            Node* tail = head;
+            for (size_t i = 1; i < arr.size(); ++i) {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+        }
+
+        Solution ob;
+        Node* ans = ob.insertAtEnd(head, x);
+        printList(ans);
+        cout << "~\n";
+    }
+    return 0;
+}
